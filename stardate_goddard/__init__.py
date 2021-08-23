8a80c49db70d85c8f9273809d80f350b6cad2b2c
thumperward <thumperward@hotmail.com>
2021-08-23 02:21:30 +0000

    use new calculation scheme for TNG/DS9/VOY

--- stardate_goddard/__init__.py
+++ stardate_goddard/__init__.py
@@ -6,43 +6,24 @@ import time
 
 
 def get_stardate(date=None, verbose=False):
+    # Calculations taken from stardate script by Phillip L. Sublett
+    # <TrekMaster@TrekGuide.com>
+    # http://trekguide.com/Stardates.htm#TNG
     requested_date = datetime.now() if date is None else parse(date)
-    origin_date = parse("1987-07-15T00:00:00-00:00")
+    origin_date = parse("2318-07-05T00:00:00-00:00")
 
     if verbose:
         print(f"Start Date : " + requested_date.strftime("%Y-%m-%d %H:%M:%S"))
         print(f"Origin Date : " + origin_date.strftime("%Y-%m-%d %H:%M:%S"))
-        print("---------------------")
-        print(f'Year : {requested_date.strftime("%Y")}')
-        print(f'Month : {requested_date.strftime("%m")}')
-        print(f'Day : {requested_date.strftime("%d")}')
-        print(f'Hour : {requested_date.strftime("%H")}')
-        print(f'Minutes : {requested_date.strftime("%M")}')
-        print(f'Seconds : {requested_date.strftime("%S")}')
-        print("---------------------")
-
     stardate = time.mktime(requested_date.timetuple()) - time.mktime(
         origin_date.timetuple()
     )
     if verbose:
         print(f"Selection Date - Origin Date = {str(stardate)}")
-        print("---------------------")
-
-    stardate = stardate / (60.0 * 60.0 * 24.0 * 0.036525)
+    stardate = stardate / 34367.0564
+    stardate = math.floor(stardate * 10) / 10
     if verbose:
-        print(f"Previous Value / (60 * 60 * 24 * 0.036525) = {str(stardate)}")
-        print("---------------------")
-    stardate = math.floor(stardate + 410000.0)
-    if verbose:
-        print(f"Floor(Previous Value + 410000.0) = {str(stardate)}")
-        print("---------------------")
-    stardate = stardate / 10.0
-    if verbose:
-        print(f"Previous Value / 10.0 = {str(stardate)}")
-        print("---------------------")
-
-    if verbose:
-        print("Stardate Final : " + str(stardate))
+        print(f"Previous Value / 34367056.4 and floored = {str(stardate)}")
     return stardate
 
 
@@ -64,9 +45,7 @@ def main():
     )
     args = parser.parse_args()
 
-    if args.verbose:
-        print("---Initializing---")
-        if args.date is None:
-            print("Using current date")
+    if args.verbose and args.date is None:
+        print("Using current date")
 
     print(get_stardate(args.date, args.verbose))
